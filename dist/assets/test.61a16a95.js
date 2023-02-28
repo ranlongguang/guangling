59f42d1ec1a172d011008dddecbb927cc963185d
ranlongguang <2314530640@qq.com>
2023-02-28 21:20:29 +0800

    上传dist文件

---
+++ dist/assets/test.61a16a95.js
@@ -0,0 +1,1 @@
+import{d as r,r as o,b as m,w as i,o as p,c as d,t as _,u as t}from"./index.4e6a148b.js";const B=r({__name:"test",setup(f){let u=o("\u8BED\u6CD5\u5B66\u4E60"),s="\u975E\u54CD\u5E94\u5F0F",e=o(0);const n=()=>{e.value++},c=m(()=>e.value*2);return i(e,(a,l)=>{console.log(999,a)},{immediate:!0}),(a,l)=>(p(),d("div",{onClick:n},_(t(u)+t(s)+t(e)+t(c)),1))}});export{B as default};
